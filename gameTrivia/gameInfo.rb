45d4b7613f2b5b068da175ae570d69c42614be93
tmadau <git@gitlab.uber5.com:tmadau>
2020-10-16 16:48:59 +0200

    modified some text input and used EOM + added 1 question

--- gameTrivia/gameInfo.rb
+++ gameTrivia/gameInfo.rb
@@ -1,3 +1,4 @@
+# info.rb
 welcome = <<EOM
 -------------------------------------------
 \\          Welcome to Game-Trivia         /
@@ -8,13 +9,10 @@ welcome = <<EOM
 -------------------------------------------
 EOM
 
-puts welcome
-typed = gets.chomp
-
 correct = <<EOM
 \e[H\e[2J
 -------------------------------------------
-[ correct ✅ \a                             ]
+[ correct ✅                              ]
 -------------------------------------------
 EOM
 
@@ -45,28 +43,10 @@ what was JavaScript originally called?
 -------------------------------------------
 EOM
 
-case typed
-when "c", "continue", "proceed"
-    puts "question 1: "
-    puts q1
-    a1 = gets.chomp
-    if a1 == "south africa"
-      puts correct
-    elsif a1 != "south africa"
-      puts wrong
+class GameInfo
+    def initialize
+        puts "this is a class initializer"
     end
-    puts "question 2: "
-    puts q2
-    a2 = gets.chomp
-    puts a2 == "ruby" ? correct : wrong
-    puts q3
-    a3 = gets.chomp
-    puts a3 == "LiveScript" ? correct : wrong
-    exit
-when "continue"
-when "exit", "x", "e"
-    puts "game exited: "
-    exit
-else ""
-    puts "unrecognized input"
 end
+
+_gameInfo = GameInfo.new
--- gameTrivia/learning.rb
+++ gameTrivia/learning.rb
@@ -1,34 +1,34 @@
-# puts "Type a string: "
-# here = gets.to_s
-# puts "type any number now: "
-# _here = gets.to_i
-# puts "type a decimal number: "
-# here1 = gets.to_f
-# puts "__________________________________________"
-# puts "------------------------------------------"
-# puts "string typed : " + here.to_s
-# puts "------------------------------------------"
-# puts "number typed : " + _here.to_s
-# puts "------------------------------------------"
-# puts "decimal typed: " + here1.to_s
-# puts "------------------------------------------"
-# puts "__________________________________________"
-# puts "------------------------------------------"
-print "please greet with your language: "
-typed = gets.chomp
-typedAgain = gets.chomp
-case typed
-when "hello", "Hello", "HELLO"
-    puts "You are English right?"
-    exit
-when "no", "NO", "No"
-    puts "Your liar"
-when "Hola", "hola", "HOLA"
-    puts "you are Spanish right?"
-    exit
-when "bonjour", "Bonjour", "BONJOUR"
-    puts "you are French right?"
-    exit 
-else "exit"
-    puts "bye!"
-end
+welcome = <<EOM
+-------------------------------------------
+\\          Welcome to Game-Trivia         /
+ \\     type continue or c to proceed.    /
+ /                                       \\
+/         To exit type exit or x          \\
+-------------------------------------------
+EOM
+
+correct = <<EOM
+-------------------------------------------
+[ correct ✅                              ]
+-------------------------------------------
+EOM
+
+wrong = <<EOM
+-------------------------------------------
+[ wrong ❌                                ]
+-------------------------------------------
+EOM
+
+q1 = <<EOM
+-------------------------------------------
+which country performed the first heart
+surgery or operation?
+-------------------------------------------
+EOM
+
+q2 = <<EOM
+-------------------------------------------
+which programming language has a framework
+called ruby on rails?
+-------------------------------------------
+EOM
